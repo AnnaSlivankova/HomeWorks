f76a572348e48a52279fea2b51e89446404a520d
Anna Slivankova <annslivankova@gmail.com>
2022-11-29 17:08:04 +0300

    Home Work #7

--- src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
+++ src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
@@ -37,7 +37,10 @@ const SuperSelect: React.FC<SuperSelectPropsType> = ({
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
         // делают студенты
+        if(onChange) onChange(e)
+        onChangeOption?.(+e.currentTarget.value)
     }
+
 
     const finalSelectClassName = s.select + (className ? ' ' + className : '')
 
